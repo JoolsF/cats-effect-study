7f41424925578dd4436be341fe970868bdae421a
Julian Fenner <joolsmf@gmail.com>
2020-09-05 11:06:59 +0100

    Error handling with IO examples

---
+++ src/main/scala/com/joolsf/errors/ErrorHandlingExample1.scala
@@ -0,0 +1,69 @@
+package com.joolsf.errors
+
+import cats.effect.IO
+import com.joolsf.errors.ErrorHandlingExample1.ErrorCaseOne
+
+import scala.concurrent.Future
+import scala.util.control.NonFatal
+
+object ErrorHandlingExample1 {
+
+  import Helpers._
+
+  // Errors
+  trait ServiceError extends Throwable {
+    val statusCode: Int
+    val msg: String
+  }
+
+  case class ErrorCaseOne(msg: String) extends ServiceError {
+    val statusCode = 500
+  }
+
+  // Examples a
+  val succeed: IO[Int] = fromFuture(future(1, false))
+  val fail: IO[Int] = fromFuture(future(2, true))
+
+  val program1: IO[Int] = fail
+    .handleErrorWith {
+      case e: ServiceError => IO(0)
+      case NonFatal(msg)   => IO(-1)
+    }
+
+  val program2: IO[Either[String, Int]] = fail.attempt
+    .map {
+      case Right(i)               => Right(i)
+      case Left(se: ServiceError) => Right(-1)
+      case e                      => Left(s"error $e")
+    }
+
+  // Examples b - Composition and fail fast behaviour
+
+  val program3: IO[Int] = {
+    for {
+      res1 <- fromFuture(future(1, false))
+      _ <- putStrLn("res 1")
+      res2 <- fromFuture(future(2, false))
+      _ <- putStrLn("res 2")
+      res3 <- fromFuture(future(3, true))
+      _ <- putStrLn("res 3")
+      res4 <- fromFuture(future(4, false))
+      _ <- putStrLn("res 4")
+    } yield res1 + res2 + res3 + res4
+  }.handleErrorWith { case e: ServiceError => IO(-1) }
+
+}
+
+object Helpers {
+  // Helpers
+  def putStrLn(str: String): IO[Unit] = IO(println(str))
+
+  def future(i: Int, fail: Boolean): Future[Int] =
+    if (fail)
+      Future.failed(ErrorCaseOne("failure"))
+    else
+      Future.successful(i)
+
+  def fromFuture[A](f: => Future[A]): IO[A] =
+    IO.fromFuture(IO(f))
+}
